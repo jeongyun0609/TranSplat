fd7ebc4d41e7c9f5d3339e2f9b88405708fe1ec8
jeongyun0609 <jungyun0609@gmail.com>
2025-04-16 17:45:08 +0900

    change ckpt link

--- Download_ckpt.sh
+++ Download_ckpt.sh
@@ -1,7 +1,8 @@
 ##SurfEmb stable-diffusion model ckpt
-gdown https://drive.google.com/uc?id=1tM91pEvN63fkpeAZE-QoRC1yZM1W-ULT
-mv SurfEmb_sd.ckpt stage_1/models
+gdown https://drive.google.com/uc?id=1KrMFEAQorhVmfAnldkamsEdmbe13w_8I
+mv SurfEmb_sd.ckpt stage_1/models/SurfEmb_sd.ckpt
 
 ##SurfEmb TRansPose model ckpt
+mkdir stage_0/SurfEmb/data/model
 gdown https://drive.google.com/uc?id=1efbYI069_ZGUuXeYEiL-k0a7AH6uSikj
-mv TRansPose-SurfEmb.ckpt stage_0/SurfEmb/data/model
+mv TRansPose-SurfEmb.ckpt stage_0/SurfEmb/data/model/TRansPose-SurfEmb.ckpt
