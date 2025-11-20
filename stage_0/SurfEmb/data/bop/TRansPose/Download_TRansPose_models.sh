c10c30cbaa401a3f3adf0c8e024ac9886c5fcfd3
HFTshoon <69518220+HFTshoon@users.noreply.github.com>
2025-11-20 17:53:25 +0900

    change download link

--- stage_0/SurfEmb/data/bop/TRansPose/Download_TRansPose_models.sh
+++ stage_0/SurfEmb/data/bop/TRansPose/Download_TRansPose_models.sh
@@ -1,5 +1,5 @@
 #TRansPose obj model
-gdown https://drive.google.com/uc?id=1jNWubrAJU3ZD8B-pmmwo3Psvx4NQg0EL
+gdown https://drive.google.com/uc?id=1qIGdd0b9S2ZQ6-C5jjGuEMegghGmDXj4
 
 tar -xvzf models.tar.gz
 
@@ -7,7 +7,7 @@ rm models.tar.gz
 
 
 #TRansPose ply model
-gdown https://drive.google.com/uc?id=1HXNhZPQfBLTapYIjHD--ros2uAhvHS1M
+gdown https://drive.google.com/uc?id=1s6FU6nRtKC7zr0lkXooUVPNAEz11FeeX
 
 tar -xvzf models_ply.tar.gz
 
